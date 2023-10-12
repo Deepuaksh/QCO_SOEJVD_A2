46a030150922bf320dc1aab1d3d4021b66656212
Akash Patil <Akash Patil@Akash>
2023-10-12 16:32:22 +0530

    Assignment

--- src/test/java/Assignment/amazon.java
+++ src/test/java/Assignment/amazon.java
@@ -3,39 +3,38 @@ package Assignment;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
-
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class amazon {
+public class amazon 
+{
 
-	public static void main(String[] args) throws InterruptedException 
+	public static void main(String[] args) throws Throwable 
 	{
 		WebDriverManager.edgedriver().setup();
 		WebDriver driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);	
+		
 		driver.get("https://www.amazon.in/");
-		String parent = driver.getWindowHandle();
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("mobile");
-		Thread.sleep(3000);
-		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//span[text()='Samsung Galaxy S22 Ultra 5G (Burgundy, 12GB, 256GB Storage) with No Cost EMI/Additional Exchange Offers']")).click();
+		driver.findElement(By.id("twotabsearchtextbox")).sendKeys("mobile");
+		driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+		driver.findElement(By.xpath("//span[text()='iQOO Neo 7 Pro 5G (Dark Storm, 8GB RAM, 128GB Storage) | Snapdragon® 8+ Gen 1 | Independent Gaming Chip | Flagship 50MP OIS Camera | AG Glass Design']")).click();
+		String parent = driver.getWindowHandle();
 		Set<String> child = driver.getWindowHandles();
 		for(String b:child)
 		{
 			driver.switchTo().window(b);
 		}
-		WebElement value = driver.findElement(By.xpath("//span[@class='a-size-large product-title-word-break']/../../../../..//span[@class='a-price a-text-price']"));
+		WebElement value = driver.findElement(By.xpath("//span[@id='productTitle']/../../../../../..//span[@class='a-size-small a-color-secondary aok-align-center basisPrice']"));
 		System.out.println(value.getText());
-		Thread.sleep(2000);
-		driver.findElement(By.xpath("//input[@id='add-to-cart-button']")).click();
-		Thread.sleep(3000);
 		driver.switchTo().window(parent);
+		
 	}
 
 }
